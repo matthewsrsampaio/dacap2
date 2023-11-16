77c073e8566aa194f7dc8f97b2e85d3448fcfaf6
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2023-11-15 23:36:08 -0300

    atualizei ate o item h + o item L

--- src/main/java/bean/JogoBean.java
+++ src/main/java/bean/JogoBean.java
@@ -4,18 +4,29 @@ import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
-import org.primefaces.PrimeFaces;
+import org.primefaces.event.RowEditEvent;
 
 import dao.JogoDao;
 import entidades.Jogo;
 
 @ManagedBean
+@ViewScoped
 public class JogoBean {
 	
 	private Jogo jogo = new Jogo();
 	private List<Jogo> listaJogada;
+	
+	public void onRowEdit(RowEditEvent<Jogo> event) throws Exception {	
+		JogoDao.editar(event.getObject());
+	    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("ID: " + event.getObject().getId()+"   ->   Jogadores Editados: ", String.valueOf(event.getObject().getTime1() + " e " + event.getObject().getTime2())));
+    }
+	
+    public void onRowCancel(RowEditEvent<Jogo> event) throws Exception {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "ID: " + event.getObject().getId()+"   ->   Edição cancelada: ", String.valueOf(event.getObject().getTime1() + " e " + event.getObject().getTime2())));
+    }	
 	
 	public String salvar() throws Exception {
 		if(!jogo.getTime1().equalsIgnoreCase(jogo.getTime2())) {
@@ -34,6 +45,16 @@ public class JogoBean {
         jogo.setGolsTime1(null);
         jogo.setGolsTime2(null);
     }
+	
+	public List<Jogo> excluir() throws Exception {
+		try {
+			JogoDao.excluir(jogo);
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Jogo "+ jogo.getId().toString() + " excluído com sucesso!", ""));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return listaJogada = JogoDao.listar();
+	}
 
 	public Jogo getJogo() {
 		return jogo;
